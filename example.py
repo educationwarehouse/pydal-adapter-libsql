a5af7706a4c2bffdab05ea5bc03c55f71f70e2ba
Robin van der Noord <robinvandernoord@gmail.com>
2023-01-09 14:33:21 +0100

    docs(import): minor improvements
    pt2

--- example.py
+++ example.py
@@ -6,6 +6,9 @@ import pydal_rqlite
 
 # db = DAL("rqlite://user:pass@localhost:4001", folder="database", driver_args={'https': False})
 db = DAL("rqlite://localhost", folder="database")
+# or with basic auth/custom port/https:
+# db = DAL("rqlite://user:pass@localhost:4001", folder="database", driver_args={'https': True})
+# db now works similarly to sqlite:
 # db = DAL("sqlite://:memory:", folder="database")
 
 db.define_table(
