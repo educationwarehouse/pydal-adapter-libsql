42ac314806dd9bb2a65af2b9721f0edc2ede8345
Robin van der Noord <robinvandernoord@gmail.com>
2023-01-09 14:29:48 +0100

    refactor(pkg): changed to package structure

---
+++ README.md
@@ -0,0 +1,17 @@
+# rqlite for pyDAL
+
+Example usage:
+
+```python
+from pydal import DAL
+
+# required to register rqlite://
+from adapter_rqlite import Rqlite
+
+# db = DAL("rqlite://user:pass@localhost:4001", folder="database", driver_args={'https': False})
+db = DAL("rqlite://localhost", folder="database")
+# db = DAL("sqlite://:memory:", folder="database")
+```
+
+`db` should now support the same features the `sqlite3` driver
+for [pyDAL](http://www.web2py.com/books/default/chapter/29/06/the-database-abstraction-layer) does.
---
+++ example.py
@@ -0,0 +1,26 @@
+from pydal import DAL
+
+# required!!
+import pydal_rqlite
+
+# db = DAL("rqlite://user:pass@localhost:4001", folder="database", driver_args={'https': False})
+db = DAL("rqlite://localhost", folder="database")
+# db = DAL("sqlite://:memory:", folder="database")
+
+db.define_table(
+    "person",
+    db.Field("name", "string"),
+    db.Field("age", "integer"),
+    db.Field("last_name", "string"),
+    # fake_migrate=True,
+)
+
+db.commit()
+
+db.person.truncate()
+
+db.person.insert(name="Henk", age=33)
+
+print(
+    db(db.person.name == "Henk").select().as_list()
+)
